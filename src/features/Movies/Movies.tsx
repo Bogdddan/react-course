971049524aa5d80d0c25e99693cea4ee0f89581b
Bogdan <bogdankrajnik95@gmail.com>
2024-04-11 17:29:28 +0300

    routing + redux  using react ts

--- src/features/Movies/Movies.tsx
+++ src/features/Movies/Movies.tsx
@@ -1,5 +1,35 @@
-export function Movies() {
+import { Movie } from "../../reducers/movies";
+import { connect } from "react-redux";
+import { RootState } from "../../store";
+import { Moviecard } from "./MovieCard";
+
+import "./Movies.css";
+
+interface MoviesProps {
+  movies: Movie[];
+}
+
+function Movies({ movies }: MoviesProps) {
   return (
-    <>Movies</>
+    <section>
+      <div className="Movies-list">
+        {movies.map((m) => (
+          <Moviecard
+            key={m.id}
+            id={m.id}
+            title={m.title}
+            overview={m.overview}
+            popularity={m.popularity} />
+        ))}
+      </div>
+    </section>
   )
 }
+
+const mapStateYoProps = (state: RootState) => ({
+  movies: state.movies.top
+});
+
+const connector = connect(mapStateYoProps);
+
+export default connector(Movies);
